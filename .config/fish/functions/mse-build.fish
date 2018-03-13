05fb157ee0e67d52da5a3d71adcb7481b1a41ea2
Jamie Magee <jamie.magee@gmail.com>
2018-03-13 15:23:50 +0000

    Add mse-build fish function

---
+++ .config/fish/functions/mse-build.fish
@@ -0,0 +1,7 @@
+function mse-build
+        set LOG_FILE "/tmp/mse_build_"(date +%Y-%m-%d_%H%M%S)".log"
+        echo "Build in progress..."
+        spin "mvn -T 1C -Dgwt.compiler.localWorkers=3 -DskipDebianPackaging=true -Dsindbad.profile=dev -DskipJavadoc=true -DargLine="-Xmx2048m" -P dev clean install > $LOG_FILE 2>&1"
+        tail $LOG_FILE
+end
+
