e78769de56f0e1301972249e3aa7209166e8e626
Jamie Magee <jamie.magee@gmail.com>
2018-04-20 17:27:03 +0200

    Decrypt encrypted dotfiles if they were updated

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -2,4 +2,8 @@ function update --description "Shorthand to update all parts of my system"
     pikaur -Syu
     fisher update
     yadm up
+    yadm diff master@\{1\} --name-only | grep "files.gpg" >/dev/null
+    if [ $status = 0 ]
+        yadm decrypt
+    end
 end
