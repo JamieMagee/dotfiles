ebab3b8d9845f8fd24fccb89b58081ba0dc0f47a
Jamie Magee <jamie.magee@gmail.com>
2018-04-20 13:31:06 +0200

    Always use fancy shell for pipenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
-set -g theme_display_cmd_duration no
-set -g theme_nerd_fonts yes
-set -g theme_color_scheme zenburn
-set -g theme_display_date no
+# Set environment variables
+source ~/.config/fish/set.fish
+
 rvm default
---
+++ .config/fish/set.fish
@@ -0,0 +1,8 @@
+# bobthefish settings
+set -g theme_display_cmd_duration no
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme zenburn
+set -g theme_display_date no
+
+# pipenv settings
+set -gx PIPENV_SHELL_FANCY 1
