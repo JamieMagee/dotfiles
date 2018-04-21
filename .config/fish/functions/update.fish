cefcc66876287c32964f689920f045dd692c4c23
Jamie Magee <jamie.magee@gmail.com>
2018-04-21 16:08:38 +0200

    Only decrypt dotfiles if changed in the last minute

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -2,7 +2,7 @@ function update --description "Shorthand to update all parts of my system"
     pikaur -Syu
     fisher update
     yadm up
-    yadm diff master@\{1\} --name-only | grep "files.gpg" >/dev/null
+    yadm diff master@\{1.minute.ago\} --name-only | grep "files.gpg" >/dev/null
     if [ $status = 0 ]
         yadm decrypt
     end
