98886aa02090f2331080c262267f66c44d766954
Jamie Magee <jamie.magee@gmail.com>
2018-05-02 08:06:05 +0200

    Use Material theme for VSCode

--- .config/fish/set.fish
+++ .config/fish/set.fish
@@ -1,7 +1,7 @@
 # bobthefish settings
 set -g theme_display_cmd_duration no
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme base16-dark
+set -g theme_color_scheme terminal
 set -g theme_display_date no
 
 # pipenv settings
