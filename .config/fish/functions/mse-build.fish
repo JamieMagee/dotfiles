cc27f5be3dde3ec6aeb63939bf1b788b2674f068
Jamie Magee <jamie.magee@gmail.com>
2018-07-05 14:32:39 +0200

    Update mse-build command

--- .config/fish/functions/mse-build.fish
+++ .config/fish/functions/mse-build.fish
@@ -1,7 +1,7 @@
 function mse-build -d "An efficient maven build command"
     set LOG_FILE "/tmp/mse_build_"(date +%Y-%m-%d_%H%M%S)".log"
     echo "Build in progress..."
-    spin "mvn -T 1C -DskipDebianPackaging=true -Dsindbad.profile=dev -DskipJavadoc=true -DargLine=\"-Xmx2048m\" -P dev clean package > $LOG_FILE 2>&1"
+    spin "mvn -T 1C -DskipDebianPackaging=true -Dsindbad.profile=dev -DskipJavadoc=true -DargLine=\"-Xmx6g\" -P dev clean package > $LOG_FILE 2>&1"
     tail $LOG_FILE
 end
 
