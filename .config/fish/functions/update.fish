a1469f7d405d91f9b3a7160a752c08d655abff02
Jamie Magee <jamie.magee@gmail.com>
2018-07-16 14:29:46 +0200

    Migrate to yay completely

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,5 +1,5 @@
 function update --description "Shorthand to update all parts of my system"
-    pikaur -Syu
+    yay -Syu
     fisher update
     yadm up
     yadm diff master@\{1.minute.ago\} --name-only | grep "files.gpg" >/dev/null
