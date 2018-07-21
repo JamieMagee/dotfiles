fa4330c57c53abe1c63ecd06c8c1ca2d16719ce4
Jamie Magee <jamie.magee@gmail.com>
2018-07-21 21:45:31 +0200

    Set key permissions correctly

--- .config/fish/functions/lpass-init-ssh.fish
+++ .config/fish/functions/lpass-init-ssh.fish
@@ -5,6 +5,7 @@ function lpass-init-ssh
   set -lx SSH_KEYS (lpass ls SSH/ | tail -n +2 | awk '{print substr($1,5,length($1));}')
   for key in $SSH_KEYS
     lpass show SSH/"$key" --field="Private Key" >> ~/.ssh/keys/"$key"
+    chmod 0600 ~/.ssh/keys/"$key"
     lpass show SSH/"$key" --field="Public Key" >> ~/.ssh/keys/"$key".pub
   end
 end
