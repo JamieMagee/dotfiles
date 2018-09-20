622704bccba6a8263af23f90143b7e6ce6389f8b
Jamie Magee <jamie.magee@gmail.com>
2018-09-20 10:27:22 +0200

    Update pinentry

--- .config/fish/lpass-pgp-pinentry.fish
+++ .config/fish/lpass-pgp-pinentry.fish
@@ -4,11 +4,22 @@ echo OK
 
 while read -la cmd
     switch "$cmd"
+
         case GETPIN
-            echo "D "(lpass show PGP/keybase --field="Passphrase")
+            if test "$passphrase" = 1
+                echo "D "(lpass show 'Secure Notes'/yadm --notes)
+            else
+                echo "D "(lpass show PGP/keybase --field="Passphrase")
+            end
             echo OK
-        case BYE
+
+        case 'SETDESC *'
+            if string match -q -r -i -- ".*enter( this)? passphrase.*" "$cmd"
+                set -g passphrase 1
+            end
             echo OK
+
+        case
         case '*'
             echo OK
     end
