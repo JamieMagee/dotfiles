3251aa27a47054e2380037cf3f5f6567fe634bd4
Jamie Magee <jamie.magee@gmail.com>
2020-12-01 16:49:20 +0100

    [fish] accept args to rdp

--- .config/fish/functions/rdp.fish
+++ .config/fish/functions/rdp.fish
@@ -2,9 +2,10 @@ function rdp -d "Connect to my VM"
   set -lx user (lpass show "microsoft.com" --field=user)
   set -lx password (lpass show "microsoft.com" --password)
   set -lx domain (lpass show "microsoft.com" --field=domain)
-  set -lx host (lpass show "microsoft.com" --field=rdp1)
+  
+  test -n "$argv"; and set -lx host "$argv".redmond.corp.microsoft.com.; or set -lx host (lpass show "microsoft.com" --field=rdp1);
 
-  set -lx ip (ip addr show | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
+  echo "$host"
 
   xfreerdp                        \
     /aero                         \
