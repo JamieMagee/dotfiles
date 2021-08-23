b1a2f7c73f2ccdb21106f623cf7aaad0da12062a
Jamie Magee <jamie.magee@gmail.com>
2021-08-23 12:44:06 +0100

    [fish] updated set

--- .config/fish/set.fish
+++ .config/fish/set.fish
@@ -1,27 +1,5 @@
-# bobthefish settings
-set -g theme_display_cmd_duration no
-set -g theme_nerd_fonts yes
-set -g theme_color_scheme terminal
-set -g theme_display_date no
-set -g theme_display_k8s_context yes
-
-# lastpass settings
-set -gx LPASS_AGENT_TIMEOUT 0
-
-# pipenv settings
-set -gx PIPENV_SHELL_FANCY 1
-
 # ruby settings
 set -gx GEM_HOME (ruby -e 'print Gem.user_dir')
-
-# maven settings
-set -gx MAVEN_OPTS "-Xmx1024M -Xss128M -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=1024M -XX:+CMSClassUnloadingEnabled"
-
-# pdsh settings
-set -gx PDSH_SSH_ARGS_APPEND "-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PreferredAuthentications=publickey"
-
-# graal
-set -gx GRAALVM_HOME /usr/lib/jvm/java-8-graal
 
 # global settings
 set -gx EDITOR nano
